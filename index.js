9bd3684ccc008c4abde0eb4792728f39b83e0f69
Anupama_Halder <anupamahalder2022@gmail.com>
2024-01-17 09:52:27 +0530

    Created hard code user data and a get API to send users data to server

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const express  = require('express');
 const app = express();
 const port = process.env.PORT || 5050;
 
+// creating hard coded user data 
+const users = [
+    {id:1, name:"anupama", email:"xyz@gmail.com"},
+    {id:2, name:"riya", email:"riya@gmail.com"},
+    {id:3, name:"priya", email:"priya@gmail.com"},
+    {id:4, name:"tina", email:"tina@gmail.com"},
+    {id:5, name:"mina", email:"mina@gmail.com"},
+]
+
+// default root route 
 app.get('/',(req,res)=>{
     res.send('Server is running!');
+})
+
+// create a get api to get users data 
+app.get('/users',(req,res)=>{
+    res.send(users);
 })
 
 // To make this server runs we have to use listen with an optional callback function
